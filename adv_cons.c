a91367041a53ee437a121de12ea004e120caa5d2
Zhen Fu <Zhen.Fu@silabs.com>
2018-08-08 16:48:56 +0800

    update to support extended adv

--- adv_cons.c
+++ adv_cons.c
@@ -7,10 +7,14 @@
  * Do not forget to modify the second parameter of gecko_cmd_le_gap_start_advertising function to le_gap_user_data
  */
 
-void setup_adv(void) {
+void setup_adv(adv_type_t adv_type) {
 	const uint8_t flag_data = 0x6;
 	const uint8_t local_name_data[] = "AdvC";
 	const uint8_t *service_uuid = bg_gattdb_data.uuidtable_128;
+	uint8_t extended_buf[150];
+
+	for(uint8_t i=0; i<150; i++)
+		extended_buf[i] = (i/10)%10 + '0';
 
 	/* https://www.bluetooth.com/specifications/assigned-numbers/company-identifiers - To get your company ID*/
 	/* Below is an example to construct your manufacturer specific data with payload set to "KBA - Adv Constructor" */
@@ -21,48 +25,63 @@ void setup_adv(void) {
 
 	ad_element_t ad_elements[] = {
 			{
-					.ad_type = flags,
+					.element_type = flags,
 					.len = 1,
 					.data = &flag_data },
 			{
-					.ad_type = complete_local_name,
+					.element_type = complete_local_name,
 					.len = sizeof(local_name_data) - 1,
 					.data = local_name_data },
 			{
-					.ad_type = more_128_uuids,
+					.element_type = more_128_uuids,
 					.len = 16,
 					.data = service_uuid },
 			{
-					.ad_type = manufacturer_specific_data,
+					.element_type = manufacturer_specific_data,
 					.len = sizeof(manu_data),
 					.data = manu_data } };
 
-	/*Set up advertisement payload*/
-	adv_t adv = {
-			.adv_handle = 0,
-			.adv_packet_type = adv_packet,
-			.ele_no = 3,
-			.p_element = ad_elements };
-	construct_adv(adv);
+	if(adv_type == legacy){
+		/*Set up advertisement payload*/
+		adv_t adv = {
+				.adv_handle = 0,
+				.adv_type = legacy,
+				.adv_packet_type = adv_packet,
+				.ele_no = 3,
+				.p_element = ad_elements };
+		construct_adv(adv);
 
-	/*Set up scan response payload*/
-	adv.adv_handle = 0;
-	adv.adv_packet_type = scan_rsp;
-	adv.ele_no = 1;
-	adv.p_element = &ad_elements[3];
-	construct_adv(adv);
+		/*Set up scan response payload*/
+		adv.adv_handle = 0;
+		adv.adv_type = legacy;
+		adv.adv_packet_type = scan_rsp;
+		adv.ele_no = 1;
+		adv.p_element = &ad_elements[3];
+		construct_adv(adv);
+	}else{
+		ad_elements[3].len = 150;
+		ad_elements[3].data = extended_buf;
+		adv_t adv = {
+				.adv_handle = 0,
+				.adv_type = extended,
+				.adv_packet_type = adv_packet,
+				.ele_no = 4,
+				.p_element = ad_elements };
+		construct_adv(adv);
+	}
 }
 
 adv_cons_ret_t construct_adv(adv_t adv) {
 	uint8_t amout_bytes = 0, i = 0;
-	uint8_t buf[31];
-	memset(buf, 0, 31);
+	uint8_t buf[MAX_EXTENDED_ADV_LENGTH];
+	memset(buf, 0, MAX_EXTENDED_ADV_LENGTH);
 	for (i = 0; i < adv.ele_no; i++) {
 		amout_bytes += adv.p_element[i].len + 2;
 	}
-	if (amout_bytes > MAX_ADV_DATA_LENGTH) {
+	if( ((amout_bytes > MAX_LEGACY_ADV_DATA_LENGTH) && (adv.adv_type == legacy)) ||
+			((amout_bytes > MAX_EXTENDED_ADV_LENGTH) && (adv.adv_type == extended)) ) {
 #ifdef LOG_SUPPORT
-		LOGE("Adv data too long [length = %d], aborting.\r\n", amout_bytes);
+		LOGE("Adv data too long [Type = %s] [length = %d], aborting.\r\n", adv.adv_type==legacy? "Legacy":"Extended", amout_bytes);
 #endif
 		return too_long;
 	}
@@ -71,11 +90,13 @@ adv_cons_ret_t construct_adv(adv_t adv) {
 
 	for (i = 0; i < adv.ele_no; i++) {
 		buf[amout_bytes++] = adv.p_element[i].len + 1;
-		buf[amout_bytes++] = adv.p_element[i].ad_type;
+		buf[amout_bytes++] = adv.p_element[i].element_type;
 		memcpy(buf + amout_bytes, adv.p_element[i].data, adv.p_element[i].len);
 		amout_bytes += adv.p_element[i].len;
 	}
-
+#ifdef LOG_SUPPORT
+	LOGD("amount bytes = %d\r\n", amout_bytes);
+#endif
 	struct gecko_msg_le_gap_bt5_set_adv_data_rsp_t *ret = gecko_cmd_le_gap_bt5_set_adv_data(adv.adv_handle, adv.adv_packet_type, amout_bytes, buf);
 #ifdef LOG_SUPPORT
 	if (error_checking(ret->result, 0)) {
--- adv_cons.h
+++ adv_cons.h
@@ -6,7 +6,13 @@
 #include "gatt_db.h"
 
 #define LOG_SUPPORT
-#define MAX_ADV_DATA_LENGTH						31
+#define MAX_LEGACY_ADV_DATA_LENGTH				31
+#define MAX_EXTENDED_ADV_LENGTH					191
+
+typedef enum{
+	legacy,
+	extended
+}adv_type_t;
 
 typedef enum {
 	success,
@@ -44,14 +50,14 @@ typedef enum {
 	extended_inquiry_response_record = 0x10,
 	/* Manufacturer Specific Data */
 	manufacturer_specific_data = 0xFF						// Format should be 2 bytes company ID + N bytes data
-} ad_type_t;
+} element_type_t;
 
 /**
  * The advertising data is composed by elements, and each element should have below structure
  */
 typedef struct {
 	/* refer to above enums */
-	ad_type_t ad_type;
+	element_type_t element_type;
 	/* Length of the data field */
 	uint8_t len;
 	/* Payload */
@@ -61,6 +67,7 @@ typedef struct {
 typedef struct {
 	/* Used to identify the advertiser instance. If there is only one, use 0. */
 	uint8_t adv_handle;
+	adv_type_t adv_type;
 	adv_packet_type_t adv_packet_type;
 	/* Explicitly specify the number of elements */
 	uint8_t ele_no;
@@ -69,5 +76,5 @@ typedef struct {
 } adv_t;
 
 adv_cons_ret_t construct_adv(adv_t adv);
-void setup_adv(void);
+void setup_adv(adv_type_t adv_type);
 #endif
